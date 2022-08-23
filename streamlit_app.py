fbd24ab2083f3210755dca42298c6b25f22917df
aregt <simsekoglu@gmail.com>
2022-08-23 23:45:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import streamlit as st
 """
 # Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+Edit `/streamlit_app.py` to customize this apsp to your heart's desire :heart:
 
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
@@ -17,4 +17,7 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 
 with st.echo(code_location='below'):
+    cuzdanTL=10000
+    alim_orani=0.1
+    karAl_orani=0.15
     
