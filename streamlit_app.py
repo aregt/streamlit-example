2c09d4505c3dd9608d60a3e59f7e2e03366fa76b
aregt <simsekoglu@gmail.com>
2022-08-24 00:30:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 with st.echo(code_location='below'):
     cuzdanTL=st.slider("Cüzdan Bakiyesi",0,10000,step=500)
-    alim_orani=st.number_input("Yeni Alım Oranı",0.01,1)
-    karAl_orani=st.number_input("Kar alma Oranı",0.01,1)
+    alim_orani=st.number_input("Yeni Alım Oranı")
+    karAl_orani=st.number_input("Kar alma Oranı")
     a=3
     
     
