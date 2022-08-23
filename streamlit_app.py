f38094521c6ac9628bfbea45078429f898cf3b8c
aregt <simsekoglu@gmail.com>
2022-08-24 00:51:20 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ import streamlit as st
 
 import datetime as dt
 
-"""Data İmport"""
 
 #import yfinance as yf
 #data = yf.download('MSFT', start = '2012-01-01', end='2017-01-01')
@@ -15,14 +14,6 @@ import datetime as dt
 link="https://www.kaggle.com/datasets/rpaguirre/tesla-stock-price?select=Tesla.csv+-+Tesla.csv.csv"
 data=pd.read_csv("link")
 
-# Welcome to Streamlit!
-
-#Edit `/streamlit_app.py` to customize this apsp to your heart's desire :heart:
-
-#If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-#forums](https://discuss.streamlit.io).
-
-#In the meantime, below is an example of what you can do with just a few lines of code:
 
 
 
