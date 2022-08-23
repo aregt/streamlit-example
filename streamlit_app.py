190e2351c9943cc012f0f27a683b662d1d722f0e
aregt <simsekoglu@gmail.com>
2022-08-24 02:08:17 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,7 @@ for i in data["Close"]:
 st.write("İşlem Listesi (Her bir adet):  ", islem_Listesi)
 st.write("İşlem alınan fiyatları:  ", islem_Listesi_alim_fiyatlari)
 st.write("Her bir işlem için ödenen para:  ", islem_Listesi_alim_bakiyeleri)
+st.write("Cüzdanda Kalan Para:  ", cuzdanTL)
     
 
 with st.echo(code_location='below'):
