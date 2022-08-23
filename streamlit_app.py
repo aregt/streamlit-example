696e12c48cb398da311848be93e66d4ce6dbca89
aregt <simsekoglu@gmail.com>
2022-08-24 00:59:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,15 +12,15 @@ If you have any questions, checkout our [documentation](https://docs.streamlit.i
 forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+cuzdanTL=10000
+alim_orani=0.1
+karAl_orani=0.15
 
+
+islem_Listesi=np.array([])
+islem_Listesi_alim_fiyatlari=np.array([])
+alim=0
+islem_Listesi_alim_fiyatlari=islem_Listesi*islem_Listesi_alim_fiyatlari
 
 with st.echo(code_location='below'):
-    cuzdanTL=10000
-    alim_orani=0.1
-    karAl_orani=0.15
     
-    
-    islem_Listesi=np.array([])
-    islem_Listesi_alim_fiyatlari=np.array([])
-    alim=0
-    islem_Listesi_alim_fiyatlari=islem_Listesi*islem_Listesi_alim_fiyatlari
