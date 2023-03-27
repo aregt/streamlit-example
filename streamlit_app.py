c73b6a8f9207e02344a9bd0223ec3fd03ad58de5
aregt <simsekoglu@gmail.com>
2023-03-27 14:48:18 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,11 +13,11 @@ import pandas as pd
 df = pd.read_csv('Tesla.csv')
 
 # Mum grafiğini oluştur
-fig = px.candlestick(df, x='Date', open='Open', high='High', low='Low', close='Close')
-fig.update_layout(xaxis_rangeslider_visible=False)
+#fig = px.candlestick(df, x='Date', open='Open', high='High', low='Low', close='Close')
+#fig.update_layout(xaxis_rangeslider_visible=False)
 
 # Grafiği sayfaya ekle
-st.plotly_chart(fig)
+# st.plotly_chart(fig)
 
 # Close kolonunu ve State kolonunu içeren yeni bir DataFrame oluştur
 new_df = pd.DataFrame()
