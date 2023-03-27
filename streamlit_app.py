15c9c3acad7d770d80b57d903c6c95f691c2d244
aregt <simsekoglu@gmail.com>
2023-03-27 16:30:17 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,25 +16,6 @@ df = pd.read_csv('Tesla.csv - Tesla.csv.csv')
 
 ########################
 
-# Mum grafik oluşturma
-fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.1)
-fig.add_trace(go.Candlestick(x=df["Date"],
-                             open=df["Open"],
-                             high=df["High"],
-                             low=df["Low"],
-                             close=df["Close"]),
-              row=1, col=1)
-fig.update_xaxes(title_text="Tarih")
-
-# Zoom ve pan yapabilmek için figür ayarları
-fig.update_layout(dragmode="zoom",
-                  xaxis_rangeslider_visible=False)
-
-# Grafik ve tablo gösterimi
-st.plotly_chart(fig)
-st.write("## Veri Tablosu")
-state_list = df["State"].unique().tolist()
-state = st.selectbox("State", state_list)
 
 
 ########################
@@ -43,11 +24,11 @@ state = st.selectbox("State", state_list)
 
 
 # Mum grafiğini oluştur
-#fig = px.candlestick(df, x='Date', open='Open', high='High', low='Low', close='Close')
-#fig.update_layout(xaxis_rangeslider_visible=False)
+fig = px.candlestick(df, x='Date', open='Open', high='High', low='Low', close='Close')
+fig.update_layout(xaxis_rangeslider_visible=False)
 
 # Grafiği sayfaya ekle
-# st.plotly_chart(fig)
+st.plotly_chart(fig)
 
 # Close kolonunu ve State kolonunu içeren yeni bir DataFrame oluştur
 new_df = pd.DataFrame()
