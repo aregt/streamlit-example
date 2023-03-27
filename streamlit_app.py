7a134ebca60c6f98b02ca11628e6bdebc70bf43b
aregt <simsekoglu@gmail.com>
2023-03-27 16:39:43 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,23 @@ import pandas as pd
 # Veri dosyasını oku
 df = pd.read_csv('Tesla.csv - Tesla.csv.csv')
 
+# Create figure
+fig = go.Figure(data=[go.Candlestick(x=df['Date'],
+                open=df['Open'],
+                high=df['High'],
+                low=df['Low'],
+                close=df['Close'])])
+
+# Set layout
+fig.update_layout(title='Tesla Stock Price',
+                   yaxis_title='Price (USD)',
+                   xaxis_rangeslider_visible=True)
+
+# Plot figure
+st.plotly_chart(fig)
+
+
+
 # Close kolonunu ve State kolonunu içeren yeni bir DataFrame oluştur
 new_df = pd.DataFrame()
 new_df['Close'] = df['Close']
